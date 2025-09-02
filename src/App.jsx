0845a808d00574645e52f0a89899640bed60d11d
Manuel-Lauriola <manuelauriol@gmail.com>
2025-09-02 18:58:41 +0200

    [UPD] - add list of articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+const mugiwara = ["Luffy", "Zoro", "Nami", "Sanji", "Usopp", "Chopper", "Robin", "Franky", "Brook", "Jinbe"];
+
 function App() {
   return (
     <>
-      <h1>React Blog Form</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Mugiwara crew</h1>
+          </div>
+          <div className="col-12">
+            <ul className="list-unstyled">
+              {mugiwara.map((member, index) =>
+                <li className="crew" key={index}>{member}</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
